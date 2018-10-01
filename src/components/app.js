a3072a9c513b21d0371a288306c11bf5fc4a4c79
lukaasc <lucas.caltabiano@gmail.com>
2018-10-01 00:45:04 -0300

    adding support to material-ui

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { inject, observer } from 'mobx-react';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
 
 import SampleStore from '../stores/sample-store';
 
 import './app.scss';
+import Image from '../assets/images/favicon.png';
 
 @inject('sampleStore')
 @observer
@@ -16,13 +20,19 @@ class App extends React.Component {
   }
 
   render() {
-    const { sampleStore } = this.props;
+    const { classes, sampleStore } = this.props;
 
     return (
-      <div>
-        <h1>React App - {sampleStore.counter}</h1>
-        <h2>Boilerplate </h2>
-      </div>
+      <Card className={classes.root}>
+        <Typography variant="display2">React App</Typography>
+        <Typography variant="display1" className={classes.display2}>
+          Boilerplate{' '}
+        </Typography>
+        <img src={Image} className={classes.image} />
+        <Typography variant="subheading" className={classes.subheading}>
+          Time Elapsed: {sampleStore.counter} (seconds)
+        </Typography>
+      </Card>
     );
   }
 }
@@ -31,4 +41,20 @@ App.propTypes = {
   sampleStore: PropTypes.instanceOf(SampleStore)
 };
 
-export default App;
+export default withStyles({
+  root: {
+    width: '30%',
+    minHeight: 600,
+    margin: '40px auto',
+    textAlign: 'center'
+  },
+  display2: {
+    marginTop: '1rem'
+  },
+  subheading: {
+    marginTop: '10rem'
+  },
+  image: {
+    marginTop: '10rem'
+  }
+})(App);
